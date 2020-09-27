184b303cc774cc437dbd9df4803db5732f2aa7dd
Aidar5 <34908447+Aidar5@users.noreply.github.com>
2020-09-27 12:34:09 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Python для сетевых инженеров'
+project = 'Python for network engineers'
 copyright = '2015-2019, Natasha Samoylenko'
 author = 'Natasha Samoylenko'
 
@@ -41,7 +41,7 @@ templates_path = ['_templates']
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'ru'
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
